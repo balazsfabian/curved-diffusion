f6caa576023fa6b3a9b56b3cc49115672cdf8b0b
Balazs Fabian <fbalazsf@gmail.com>
2021-04-19 10:57:07 +0200

    Added wrap-msd option to mesh-info.py

--- mesh-info.py
+++ mesh-info.py
@@ -4,6 +4,8 @@ from itertools import cycle
 
 import numpy as np
 import pyvista as pv
+from sklearn.neighbors import NearestNeighbors
+
 
 def write_xyz(fout, coords, title="", atomtypes=("A",)):
     """ write a xyz file from file handle
@@ -30,20 +32,71 @@ def write_xyz(fout, coords, title="", atomtypes=("A",)):
         fout.write("%s %.18g %.18g %.18g\n" % (atomtype, x[0], x[1], x[2]))
 
 
+def wrap_msd_arrays(mesh):
+    """ wrap point-array properties into the unit cell of the mesh
+    """
+    lagtimes = mesh.field_arrays['lagtimes']
+    lx = mesh.field_arrays['lx'][0]
+    ly = mesh.field_arrays['ly'][0]
+    
+    #Set up NBsearch
+    pts = mesh.points
+    neigh = NearestNeighbors(n_neighbors=1,n_jobs=-1)
+    neigh.fit(pts)
+    
+    #Wrap points into [0,lx] x [0,ly] x [.,.]
+    periodic_pts = np.mod(pts,[lx,ly,np.inf])
+    # negative points misbehave when mod is calculated
+    periodic_pts[pts <0] = pts[pts <0]
+    
+    mapping = neigh.kneighbors(periodic_pts)[1].flatten()
+
+    for dt in lagtimes:
+        dsum = mesh.point_arrays[f'dsum={dt}']
+        csum = mesh.point_arrays[f'csum={dt}']
+    
+        for i,ndx in enumerate(mapping):
+            if i == ndx:
+                continue
+            # add to central image...
+            dsum[ndx] += dsum[i]
+            csum[ndx] += csum[i]
+            # ... and clear the non-central
+            dsum[i] = np.nan
+            csum[i] = np.nan
+    
+        csum[csum==0] = np.nan
+    
+        mesh.point_arrays[f'dsum={dt}'] = dsum
+        mesh.point_arrays[f'csum={dt}'] = csum
+    
+        mesh.point_arrays[f"lagtime={dt}"] = np.divide(dsum,csum)
+
+
 fn = sys.argv[1]
 prop = sys.argv[2]
 
 mesh = pv.read(fn)
+
+
+# Wrap properties into the unit cell
+if prop == 'wrap-msd':
+    wrap_msd_arrays(mesh)
+    mesh.save('final.vtk')
+    exit()
+
 
 # Write an obj file
 if prop == 'obj':
     pv.save_meshio(fn[:-3]+'obj',mesh)
     exit()
 
+
 # Write an xyz file
 if prop == 'xyz':
     write_xyz(open(fn[:-3]+'xyz','w'), mesh.points, title='meshed surface')
     exit()
+
 
 # Print average MSD curve
 if prop == 'msd':
@@ -56,6 +109,7 @@ if prop == 'msd':
     except KeyError:
         print ("This mesh does not contain MSD data!")
     exit(1)
+
 
 # Access generic data from any source
 data = mesh[prop]
