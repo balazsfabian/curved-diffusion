f25229090e76fbbcd016a169a48a5f12b486b19f
Balazs Fabian <fbalazsf@gmail.com>
2021-04-25 15:14:10 +0200

    'line-average.py' extended
    Now also handles both curvatures

--- line-average.py
+++ line-average.py
@@ -31,6 +31,7 @@ parser.add_argument('mesh')
 parser.add_argument('--direction', default='x',    type=CheckAverageDirection, help="Direction for averaging")
 parser.add_argument('--bins',      default=50,     type=int,                   help="Number of bins")
 parser.add_argument('--name',      default='mesh', type=str,                   help="Stem of the output names")
+parser.add_argument('--property',  default='msd',  type=str,                   help="Property to be processed")
 
 args = parser.parse_args()
 
@@ -44,39 +45,77 @@ ly = mesh.field_arrays['ly'][0]
 # project onto the xy plane
 mesh.project_points_to_plane(origin=(0,0,0), normal=(0,0,1),inplace=True)
 
-# calculate the relevant positions
-if args.direction == 'radial':
-    mesh.points[:,0] -= lx/2
-    mesh.points[:,1] -= ly/2
-    positions = np.linalg.norm(mesh.points,axis=1)
-    hist_range = [0,min(lx,ly)/2]
+# Calculate MSD for all lagtimes ------------------------------
+if args.property == 'msd':
+    # calculate the relevant positions
+    if args.direction == 'radial':
+        mesh.points[:,0] -= lx/2
+        mesh.points[:,1] -= ly/2
+        positions = np.linalg.norm(mesh.points,axis=1)
+        hist_range = [0,min(lx,ly)/2]
 
-if args.direction == 'x':
-    positions = mesh.points[:,0]
-    hist_range = [0, lx]
+    if args.direction == 'x':
+        positions = mesh.points[:,0]
+        hist_range = [0, lx]
 
-if args.direction == 'y':
-    positions = mesh.points[:,1]
-    hist_range = [0, ly]
+    if args.direction == 'y':
+        positions = mesh.points[:,1]
+        hist_range = [0, ly]
 
-# Take the non-normalized values, and bin them
-for dt in lagtimes:
-    dsum = mesh.point_arrays[f'dsum={dt}']
-    csum = mesh.point_arrays[f'csum={dt}']
 
-    # filter out NaN-s
-    mask =np.isnan(dsum)
-    dsum = dsum[~mask]
-    csum = csum[~mask]
-    p = positions[~mask]
+    # Take the non-normalized values, and bin them
+    for dt in lagtimes:
+        dsum = mesh.point_arrays[f'dsum={dt}']
+        csum = mesh.point_arrays[f'csum={dt}']
 
-    # histogram
-    hist, edges = np.histogram(p, bins=args.bins, weights=dsum, range=hist_range) 
-    norm, _     = np.histogram(p, bins=args.bins, weights=csum, range=hist_range) 
+        # filter out NaN-s
+        mask = np.isnan(dsum)
+        dsum = dsum[~mask]
+        csum = csum[~mask]
+        p = positions[~mask]
+
+        # histogram
+        hist, edges = np.histogram(p, bins=args.bins, weights=dsum, range=hist_range)
+        norm, _     = np.histogram(p, bins=args.bins, weights=csum, range=hist_range)
+
+        values = np.divide(hist,norm)
+        centers = (edges[:-1] + edges[1:])/2
+        data = np.vstack((centers,values)).T
+
+        np.savetxt(f"{args.name}-{args.property}-{args.direction}-{dt}.dat", data)
+
+
+# Calculate Mean or Gaussian curvature for all lagtimes ----------
+elif args.property in ['Mean','Gaussian']:
+    # calculate the relevant positions
+    # * use centers of the faces
+    points = mesh.cell_centers().points
+    if args.direction == 'radial':
+        points[:,0] -= lx/2
+        points[:,1] -= ly/2
+        positions = np.linalg.norm(points,axis=1)
+        hist_range = [0,min(lx,ly)/2]
+
+    if args.direction == 'x':
+        positions = points[:,0]
+        hist_range = [0, lx]
+
+    if args.direction == 'y':
+        positions = points[:,1]
+        hist_range = [0, ly]
+
+    # covert point property to cell property
+    prop = mesh.ptc().cell_arrays[args.property]
+    area = mesh.compute_cell_sizes(length=False,area=True,volume=False).cell_arrays['Area']
+    prop = np.multiply(prop,area)
+
+    # area-weighted curvature values
+    hist, edges = np.histogram(positions, bins=args.bins, weights=prop, range=hist_range)
+    norm, _     = np.histogram(positions, bins=args.bins, weights=area, range=hist_range)
 
     values = np.divide(hist,norm)
     centers = (edges[:-1] + edges[1:])/2
     data = np.vstack((centers,values)).T
 
-    np.savetxt(f"{args.name}-{args.direction}-{dt}.dat", data)
+    np.savetxt(f"{args.name}-{args.property}-{args.direction}.dat", data)
 
